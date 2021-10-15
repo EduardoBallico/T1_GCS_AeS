4fb52b8f36d16e2f3b0dc084fcf964ede206583c
Guilherme Matheus Dornelles <guilherme.dornelles@edu.pucrs.br>
2021-10-15 19:01:59 -0300

    Inclusão de novos métodos e mudança na App

--- src/App.java
+++ src/App.java
@@ -1,29 +1,48 @@
-import departamentos.DepComercial;
-import departamentos.DepEngenharia;
-import departamentos.DepFinanceiro;
-import departamentos.DepTI;
-import departamentos.Departamento;
-import models.Item;
-import models.ListaDeItens;
-import models.Pedido;
-import models.Usuario;
+import java.lang.reflect.Array;
+
+import departamentos.*;
+import models.*;
+import registros.RegistroDePedidos;
+import java.util.ArrayList;
 
 public class App {
 	public static void main(String[] args) {
-		System.out.print("Inicializando...");
-		preencheDados();
+		RegistroDePedidos registro = new RegistroDePedidos();
+		System.out.println("Inicializando...");
+		ArrayList<Pedido> listAux = preencheDados();
+		System.out.println(listAux.size());
+		for(int i=0; i<listAux.size(); i++){
+			registro.inserePedido(listAux.get(i));
+		}
+		listAux.clear();		
+		System.out.println(registro.buscaAberto());
+		
 	}
 
-	public static void preencheDados(){
-		Departamento dep1 = new DepTI(50000);
-		Departamento dep2 = new DepComercial(80000);
-		Departamento dep3 = new DepEngenharia(90000);
-		Departamento dep4 = new DepFinanceiro(100000);
+	public static ArrayList<Pedido> preencheDados(){
+		ArrayList<Pedido> listaAux = new ArrayList<Pedido>();
+		Departamento depTi = new DepTI(150000);
+		Departamento depComercial = new DepComercial(90000);
+		Departamento depEngenharia = new DepEngenharia(170000);
+		Departamento depFinanceiro = new DepFinanceiro(00);
+		Departamento depManutencao = new DepManutencao(50000);
+		Departamento depRh = new DepRH(35000);
 
-		Usuario funcionario1 = new Usuario(dep1, 1111, "Eduardo", true);
-		Usuario funcionario2 = new Usuario(dep2, 1112, "Guilherme", false);
-		Usuario funcionario3 = new Usuario(dep3, 1113, "Vinicius", false);
-		Usuario funcionario4 = new Usuario(dep4, 1114, "Arthur", true);
+		Usuario funcionario1 = new Usuario(depTi, "Eduardo", true);
+		Usuario funcionario2 = new Usuario(depComercial, "Guilherme", false);
+		Usuario funcionario3 = new Usuario(depEngenharia, "Vinicius", false);
+		Usuario funcionario4 = new Usuario(depFinanceiro, "Arthur", true);
+		Usuario funcionario5 = new Usuario(depManutencao, "André", true);
+		Usuario funcionario6 = new Usuario(depRh, "Leandro", true);
+		Usuario funcionario7 = new Usuario(depTi, "Daniel", true);
+		Usuario funcionario8 = new Usuario(depComercial, "Pedro", true);
+		Usuario funcionario9 = new Usuario(depEngenharia, "Eduarda", true);
+		Usuario funcionario10 = new Usuario(depFinanceiro, "Vitoria", true);
+		Usuario funcionario11 = new Usuario(depManutencao, "Gertrudes", true);
+		Usuario funcionario12 = new Usuario(depRh, "Carla", true);
+		Usuario funcionario13 = new Usuario(depTi, "Claudia", true);
+		Usuario funcionario14 = new Usuario(depComercial, "Marisa", true);
+		Usuario funcionario15 = new Usuario(depEngenharia, "Eveline", true);
 
 		ListaDeItens list1 = new ListaDeItens();
 		list1.incluirItem(new Item("Computador Intel i7, RTX 3080, 32gb de ram", 15000, 1));
@@ -41,10 +60,27 @@ public class App {
 		list4.incluirItem(new Item("Impressora HP Series-T", 1250, 10));
 		list4.incluirItem(new Item("Folhas A4 para impressora", 19, 200));
 
+		ListaDeItens list5 = new ListaDeItens();
+		list5.incluirItem(new Item("Kit de chaves Allen", 50, 50));
+		list5.incluirItem(new Item("Jogo de chaves Philips", 50, 100));
+		list5.incluirItem(new Item("Jogo de chaves de Fenda", 50, 100));
 
-		Pedido ped1 = new Pedido(funcionario1, dep1, list1);
-		Pedido ped2 = new Pedido(funcionario2, dep2, list2);
-		Pedido ped3 = new Pedido(funcionario3, dep3, list3);
-		Pedido ped4 = new Pedido(funcionario4, dep4, list4);
+		ListaDeItens list6 = new ListaDeItens();
+		list6.incluirItem(new Item("Folhas A4 para impressora", 19, 200));
+
+		Pedido ped1 = new Pedido(funcionario1, depTi, list1);
+		listaAux.add(ped1);
+		Pedido ped2 = new Pedido(funcionario2, depComercial, list2);
+		listaAux.add(ped2);
+		Pedido ped3 = new Pedido(funcionario3, depEngenharia, list3);
+		listaAux.add(ped3);
+		Pedido ped4 = new Pedido(funcionario4, depFinanceiro, list4);
+		listaAux.add(ped4);
+		Pedido ped5 = new Pedido(funcionario4, depManutencao, list5);
+		listaAux.add(ped5);
+		Pedido ped6 = new Pedido(funcionario4, depRh, list6);
+		listaAux.add(ped6);
+		
+		return listaAux;
 	}
 }
