c5b432376b2e31796aa746c5231a227a83dd8595
Arthur Kunzler <arthur.gabriel.k@gmail.com>
2021-10-15 18:49:47 -0300

    adiconando contrutor de registroDePedidos

--- src/registros/RegistroDePedidos.java
+++ src/registros/RegistroDePedidos.java
@@ -1,5 +1,7 @@
 package registros;
 
+import java.util.Date;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 import models.Pedido;
@@ -7,16 +9,29 @@ import models.Usuario;
 
 public class RegistroDePedidos {
 
+	public RegistroDePedidos(ArrayList<Pedido> pedidos) {
+		this.pedidos = pedidos;
+	}
+
 	private ArrayList<Pedido> pedidos;
 
-	public ArrayList<Pedido> listaData(String dataInicial, String dataFinal) {
+	// recebe datas string no formato: dd/mm/yyyy
+	public ArrayList<Pedido> listaDatasNoPeriodo(String dataInicial, String dataFinal) {
+		// informa os pedidos entre data inicial e data final
+		try {
+			SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+			Date dataInic = formatter.parse(dataInicial);
+			Date dataFin = formatter.parse(dataFinal);
+		} catch (Exception e) {
+			return null;
+		}
 		return null;
 	}
 
 	public ArrayList<Pedido> buscaSolicitante(Usuario funcionario) {
 		ArrayList<Pedido> pedSolic = new ArrayList<Pedido>();
-		for(Pedido p: pedidos){
-			if(p.getFunc() == funcionario){
+		for (Pedido p : pedidos) {
+			if (p.getFunc() == funcionario) {
 				pedSolic.add(p);
 			}
 		}
@@ -25,8 +40,8 @@ public class RegistroDePedidos {
 
 	public ArrayList<Pedido> buscaDescricao(String descricao) {
 		ArrayList<Pedido> pedDesc = new ArrayList<Pedido>();
-		for(Pedido p: pedidos){
-			if(p.getItem(descricao)){
+		for (Pedido p : pedidos) {
+			if (p.getItem(descricao)) {
 				pedDesc.add(p);
 			}
 		}
@@ -35,8 +50,8 @@ public class RegistroDePedidos {
 
 	public ArrayList<Pedido> buscaAberto() {
 		ArrayList<Pedido> pedAberto = new ArrayList<Pedido>();
-		for(Pedido p: pedidos){
-			if(!p.concluido()){
+		for (Pedido p : pedidos) {
+			if (!p.concluido()) {
 				pedAberto.add(p);
 			}
 		}
@@ -44,9 +59,9 @@ public class RegistroDePedidos {
 	}
 
 	public boolean aprovar(int codigo, Usuario usuario) {
-		for(int i=0; i<pedidos.size(); i++){
-			if(pedidos.get(i).getCodigo() == codigo){
-				if(pedidos.get(i).aprovar(usuario)){
+		for (int i = 0; i < pedidos.size(); i++) {
+			if (pedidos.get(i).getCodigo() == codigo) {
+				if (pedidos.get(i).aprovar(usuario)) {
 					return true;
 				}
 			}
@@ -55,9 +70,9 @@ public class RegistroDePedidos {
 	}
 
 	public boolean reprovar(int codigo, Usuario usuario) {
-		for(int i=0; i<pedidos.size(); i++){
-			if(pedidos.get(i).getCodigo() == codigo){
-				if(pedidos.get(i).reprovar(usuario)){
+		for (int i = 0; i < pedidos.size(); i++) {
+			if (pedidos.get(i).getCodigo() == codigo) {
+				if (pedidos.get(i).reprovar(usuario)) {
 					return true;
 				}
 			}
@@ -65,8 +80,7 @@ public class RegistroDePedidos {
 		return false;
 	}
 
-	public String estaticasGerais(Usuario usuario) {
+	public String estatisticasGerais(Usuario usuario) {
 		return null;
 	}
-
 }
