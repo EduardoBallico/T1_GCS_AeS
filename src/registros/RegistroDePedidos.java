a879568cfce6f7fe90ced8dd11a6b6fadf2d9403
Arhur Kunzler <arthur.gabriel.k@gmail.com>
2021-10-16 19:18:25 -0300

    adicionando estatisticasgerais em RegistroDePedidos - agora com o usuario certo

--- src/registros/RegistroDePedidos.java
+++ src/registros/RegistroDePedidos.java
@@ -1,23 +1,27 @@
 package registros;
 
 import java.util.Date;
+
+import departamentos.*;
+
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 import models.Pedido;
+import models.StatusPedido;
 import models.Usuario;
 
 public class RegistroDePedidos {
+	
+	private ArrayList<Pedido> pedidos;
 
 	public RegistroDePedidos(ArrayList<Pedido> pedidos) {
 		this.pedidos = pedidos;
 	}
-
-	private ArrayList<Pedido> pedidos;
-
+	
 	// recebe datas string no formato: dd/MM/yyyy
 	public ArrayList<Pedido> listaPedidosNoPeriodo(String dataInicial, String dataFinal) {
-		// informa os pedidos entre data inicial e data final
 		try {
 			SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
 			Date dataInic = formatter.parse(dataInicial);
@@ -88,8 +92,52 @@ public class RegistroDePedidos {
 	}
 
 	public String estatisticasGerais(Usuario usuario) {
-		String resp = "";
-
-		return null;
+		int pedidosAprovados = 0, pedidosReprovados = 0 , totalPedidos = 0;
+		for(Pedido p : pedidos){
+			if(p.getStatus() == StatusPedido.APROVADO) pedidosAprovados++;
+			else if (p.getStatus() == StatusPedido.REPROVADO) pedidosReprovados++;
+			totalPedidos++;
+		}
+		String totalPedidosDesc = "Total de pedidos: "+totalPedidos
+		+"\n	Pedidos aprovados: "+pedidosAprovados+"("+(((double)totalPedidos/pedidosAprovados)*100)+"%)\n	Pedidos reprovados: "+pedidosAprovados+"("+(((double)totalPedidos/pedidosReprovados)*100)+"%)";
+				
+		// essa classe Calendar.MONTH tem indexes 0-11 para os meses, por isso, se o mes for 0 colocamos 12 pois um mes antes de janeiro vem dezembro, e dimin.uimos 1 do ano atual
+		String data = Calendar.MONTH == 0 ? Calendar.DATE +"/"+ 12 +"/"+ (Calendar.YEAR-1) : Calendar.DAY_OF_WEEK +"/"+ Calendar.MONTH +"/"+ Calendar.YEAR;
+		SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+		double valorMedio = 0, pedidosNoUltimoMes = 0;
+		double valorDepComercial = 0, valorDepEngenharia = 0, valorDepFinanceiro = 0, valorDepManutencao = 0, valorDepRH = 0, valorDepTI = 0;
+		Pedido pedidoAbertoMaior = null;
+		try {
+			Date dataMesAnterior = formatter.parse(data);
+			for(Pedido p : pedidos){
+				if(dataMesAnterior.after(p.getDataPed())){
+					valorMedio += p.getValTot();
+					pedidosNoUltimoMes++;
+					if(p.getDepart() instanceof DepComercial) valorDepComercial+= p.getValTot();
+					else if(p.getDepart() instanceof DepEngenharia) valorDepEngenharia+= p.getValTot();
+					else if(p.getDepart() instanceof DepFinanceiro) valorDepFinanceiro+= p.getValTot();
+					else if(p.getDepart() instanceof DepManutencao) valorDepManutencao+= p.getValTot();
+					else if(p.getDepart() instanceof DepRH) valorDepRH+= p.getValTot();
+					else if(p.getDepart() instanceof DepTI) valorDepTI+= p.getValTot();
+				}
+				if(p.getStatus() == StatusPedido.ABERTO){
+					if(pedidoAbertoMaior == null && p.getValTot() > pedidoAbertoMaior.getValTot()){
+						pedidoAbertoMaior = p;
+					}
+				}
+			}
+			valorMedio = valorMedio / pedidosNoUltimoMes;
+		} catch (Exception e) {
+			return null;
+		}
+		String numeroPedidosDesc = "Numero de pedidos nos ultimos 30 dias: "+pedidosNoUltimoMes+"\n	Valor médio: "+valorMedio;
+		String[] departamentos = {"Departamento Comercial", "Departamento Engenharia", "Departamento Financeiro", "Departamento Manutencao", "Departamento RH", "Departamento TI"};
+		double[] valorPorDepartamento = {valorDepComercial, valorDepEngenharia, valorDepFinanceiro, valorDepManutencao, valorDepRH, valorDepTI};
+		String descDepartamentos = "";
+		for (int i = 0; i < departamentos.length; i++) {
+			descDepartamentos += "\n"+departamentos[i]+": "+valorPorDepartamento;
+		}
+		String pedidoAbertoMaiorDesc = pedidoAbertoMaior.toString();
+		return totalPedidosDesc+"\n"+numeroPedidosDesc+"\n"+descDepartamentos+"\n"+pedidoAbertoMaiorDesc;
 	}
 }
